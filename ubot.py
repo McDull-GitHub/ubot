6cb220b6e539d15c4a10e232c9c316bfa5a184fe
Mcdull <1042762344@qq.com>
2020-12-17 08:49:41 +0800

    Adjust algorithms and add usages (#1)

--- ubot.py
+++ ubot.py
@@ -6,7 +6,7 @@ api_hash = '341e29114e1bb38d1fda9f1a22b59b28'
 with TelegramClient('userbot', api_id, api_hash) as client:
     @client.on(events.NewMessage(pattern='-delit', outgoing=True))
     async def delit(event):
-        print("delit is running")
+        print("-delit\t ->\trunning")
         target = await event.get_reply_message()
         if event.reply_to_msg_id:
             try:
@@ -14,35 +14,58 @@ with TelegramClient('userbot', api_id, api_hash) as client:
             except:
                 print("delit error")
         await event.delete()
+        print("-delit\t ->\tdone")
 
     @client.on(events.NewMessage(pattern='-delall', outgoing=True))
     async def delall(event):
-        print("delall is running")
-        async for message in event.client.iter_messages(event.chat_id):
-            await message.delete()
+        print("-delall\t ->\trunning")
+        input_chat = await event.get_input_chat()
+        messages = []
+        async for message in event.client.iter_messages(input_chat):
+            if (message.id==1):
+                if messages:
+                    await event.client.delete_messages(input_chat, messages)
+                break
+            messages.append(message)
+            if len(messages) == 100:
+                await event.client.delete_messages(input_chat, messages)
+        print("-delall\t ->\tdone")
 
     @client.on(events.NewMessage(pattern='-delme', outgoing=True))
     async def delme(event):
-        print("delme is running")
-        async for message in event.client.iter_messages(event.chat_id, from_user="me"):
-            await message.delete()
+        print("-delme\t ->\trunning")
+        input_chat = await event.get_input_chat()
+        messages = []
+        loop = 0
+        async for message in event.client.iter_messages(input_chat):
+            if (message.id==1):
+                if messages:
+                    await event.client.delete_messages(input_chat, messages)
+                break
+            loop += 1
+            if (message.from_id.user_id == myid):
+                messages.append(message)
+            if loop == 100:
+                if messages:
+                    await event.client.delete_messages(input_chat, messages)
+                loop = 0
+        print("-delme\t ->\tdone")
 
     @client.on(events.NewMessage(pattern='-delfrom', outgoing=True))
     async def delfrom(event):
-        print("delfrom is running")
+        print("-delfrom ->\trunning")
         if not event.reply_to_msg_id:
             await event.delete()
             return
         input_chat = await event.get_input_chat()
         messages = []
-        count = 0
-        async for message in event.client.iter_messages(input_chat, min_id=event.reply_to_msg_id):
+        async for message in event.client.iter_messages(input_chat, min_id=event.reply_to_msg_id-1):
             messages.append(message)
-            count += 1
-            messages.append(event.reply_to_msg_id)
             if len(messages) == 100:
                 await event.client.delete_messages(input_chat, messages)
         if messages:
             await event.client.delete_messages(input_chat, messages)
-
+        print("-delfrom ->\tdone")
+    myid = client.get_me().id
+    print("Usage:\n\t-delit\t ->\tDetele a message you reply\n\t-delme\t ->\tDetele all messages you send\n\t-delall\t ->\tDetele all messages\n\t-delfrom ->\tDetele messages from where you reply\n")
     client.run_until_disconnected()
